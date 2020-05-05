0ff9a7d2f19eb9b723dcd9a31693a27d233a9262
craigkehl <keh19003@byui.edu>
2020-05-04 22:14:36 -0600

    syncing fix

---
+++ W2/Team_personal_stretch/js/app.js
@@ -0,0 +1,69 @@
+/********* Part 1  ****************/
+/** Dom Elements */
+// const usrInput = document.getElementById('number_input');
+// const btnInput = document.getElementById('btn');
+// const output = document.getElementById('output');
+
+// /** Functions */
+// const getNumberInput = () => { return usrInput.value};
+
+// const procInOut = () => {
+//   output.innerHTML = getNumberInput();
+//   console.log(getNumberInput());
+//   console.log(output.textContent);
+// } 
+
+/** main code */
+
+// console.log('Get Started');
+// btnInput.addEventListener('click', procInOut);
+
+
+
+/********** Stretch ***********/
+/** Retrieve DOM elements */
+const btnAdd = document.getElementById('btnAdd');
+const btnSub = document.getElementById('btnSub');
+const btnMult = document.getElementById('btnMult');
+const btnDiv = document.getElementById('btnDiv');
+const output = document.getElementById('output');
+
+function showOutput(message) {  //Standard function declaration
+  output.textContent = message;
+}
+
+const validate = function (testNum) {  //expression declaration
+  (isNaN(testNum)) ? alert("Input must be numeric"):
+    testNum;
+    return testNum;
+}
+
+const calculate = (op) => {  // Fat Arrow Function
+  const n1El = parseInt(document.getElementById('num1_input').value);
+  const n2El = parseInt(document.getElementById('num2_input').value);
+  const num1 = validate(n1El);
+  const num2 = validate(n2El);  
+    switch (op) {
+      case "+":
+        results = num1 + num2;
+        break;
+      case "-":
+        results = num1 - num2;
+        break;
+      case "*":
+        results = num1 * num2;
+        break;
+      case "/":
+        results = num1 / num2;
+        break;
+      default:
+        break;
+    } 
+  showOutput(results);
+};
+
+/** Event listners with callback() and .bind() */
+btnAdd.addEventListener('click', calculate.bind(this, '+'));
+btnSub.addEventListener('click', calculate.bind(this, '-'));
+btnMult.addEventListener('click', calculate.bind(this, '*'));
+btnDiv.addEventListener('click', calculate.bind(this, '/'));
---
+++ W2/notes/function.js
@@ -0,0 +1,53 @@
+/** Standard Function declaration ****
+ * Advantage: Can be declared anywhere in your code and 
+ * it will be pareced and ready when needed. */
+function var_name(pram1, pram2, ...pram) {
+  z = a + b;
+  return z;
+}
+
+/** function expression ***
+ * Can be more concise */
+
+const name = function(pram1, pram2, ...pram) { return z}
+
+
+/*** Arrow Functions (old),  Fat Arrow Functions ****/
+
+/** Default syntax: 
+ *  Same as "normal" functions, parameters and return   statement are optional. Noteworthy: Semi-colon at end, no function keyword, parentheses around parameters/ arguments.*/
+const add = (a, b) => {
+const result = a + b;
+return result;
+};  
+
+
+/** Shorter syntax, if only one parameter. Noteworthy: Parentheses around parameter list can be omitted */
+const log = message => {
+console.log(message);
+return z;
+};  
+
+
+/** Empty parameter parentheses if no arguments are receive */
+const greet = () => {
+console.log('Hi there!');
+}; 
+
+
+/** Short function body, if only one expression is used. An expression result is always returned automatically*/
+const add = (a, b) => a + b; 
+
+
+/** THe function can return an object. Extra parentheses are required around the object, since the curly braces would otherwise be interpreted as the function body delimiters */
+const loadPerson = pName => ({name: pName });  
+
+
+/** That last case can be confusing: Normally, in JavaScript, curly braces always can have exactly one meaning. */
+const person = { name: 'Max' }; // Clearly creates an object
+if (something) { ... } // Clearly used to mark the if statement block
+
+/** But when using arrow functions, curly braces can have two meanings:
+Mark the function body (in default form)
+Create an object which you want to return (in shorter function body form)
+To "tell" JavaScript what you want to do, wrap the expression (e.g. object creation) in parentheses like shown above. */
